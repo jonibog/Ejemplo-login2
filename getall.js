3d18103b5e9315923f894aa117a411db718769e2
jonibog <135062183+jonibog@users.noreply.github.com>
2023-05-30 23:42:42 -0300

    Update getall.js

--- getall.js
+++ getall.js
@@ -1,13 +1,1 @@
-const usuarios=[] 
-async function getAll(){
 
-    await fetch("http://localhost:8081/login")
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(user => {
-            usuarios.push(user);
-        });
-    });
-}
-getAll()
-console.log(usuarios)
